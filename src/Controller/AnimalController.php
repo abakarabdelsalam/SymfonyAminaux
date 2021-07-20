0d10d1ead2c15764d3746e22d77bfecc2b20ebda
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-07-20 17:27:51 +0200

    injection de repository

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Animal;
+use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +13,9 @@ class AnimalController extends AbstractController
     /**
      * @Route("/", name="animaux")
      */
-    public function index(): Response
+    public function index(AnimalRepository $repository): Response
     {
-        $repository = $this->getDoctrine()->getRepository(Animal::class);
+        //$repository = $this->getDoctrine()->getRepository(Animal::class);
         $animaux = $repository->findAll();
         return $this->render('animal/index.html.twig', [
             "listAnimaux" => $animaux
