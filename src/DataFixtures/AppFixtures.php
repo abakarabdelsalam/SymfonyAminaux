3aad836205160f773b1e06c5e01044f6b6934e02
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-07-20 16:31:13 +0200

    bootsrap card group

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,34 +14,44 @@ class AppFixtures extends Fixture
         $a1 = new Animal();
         $a1->setNom('Chien')
             ->setDescription('Anmale deomestique')
-            ->setImage('chien.png');
+            ->setImage('chien.png')
+            ->setPoids(30)
+            ->setDengereux(false);
         $manager->persist($a1);
 
 
         $a2 = new Animal();
-        $a2->setNom('Crokodil')
+        $a2->setNom('Crocodil')
             ->setDescription('Anmale sauvage')
-            ->setImage('croco.png');
+            ->setImage('croco.png')
+            ->setPoids(90)
+            ->setDengereux(true);
         $manager->persist($a2);
 
         $a3 = new Animal();
-        $a3->setNom('Sepan')
+        $a3->setNom('Sepent')
             ->setDescription('Anmale dangere')
-            ->setImage('serpan.png');
+            ->setImage('serpent.png')
+            ->setPoids(4)
+            ->setDengereux(true);
         $manager->persist($a3);
 
 
         $a4 = new Animal();
-        $a4->setNom('Chien')
+        $a4->setNom('Cochon')
             ->setDescription('Anmale deomestique')
-            ->setImage('chien.png');
+            ->setImage('cochon.png')
+            ->setPoids(100)
+            ->setDengereux(false);
         $manager->persist($a4);
 
 
         $a5 = new Animal();
         $a5->setNom('Requin')
             ->setDescription('Anmale de la mere')
-            ->setImage('requin.png');
+            ->setImage('requin.png')
+            ->setPoids(1000)
+            ->setDengereux(true);
         $manager->persist($a5);
 
 
